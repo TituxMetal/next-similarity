627b422ed4d5df2ce3bc469049e51eab4fb4158e
Titux Metal (MBP) <github@lgdweb.fr>
2023-03-16 06:28:03 +0100

    Use navbar component in root layout and update tailwind configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Inter } from 'next/font/google'
 import type { ReactNode } from 'react'
 
-import { Providers } from '~/components'
+import { Navbar, Providers } from '~/components'
 import { cn } from '~/lib'
 import '~/styles/globals.css'
 
@@ -14,7 +14,11 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
       className={cn('bg-slate-50 text-slate-900 antialiased', inter.className)}
     >
       <body className='min-h-screen bg-slate-50 antialiased dark:bg-slate-900'>
-        <Providers>{children}</Providers>
+        <Providers>
+          {children}
+          {/* @ts-expect-error Server Component */}
+          <Navbar />
+        </Providers>
         {/* Allow for more height on mobile devices */}
         <div className='h-40 md:hidden' />
       </body>
