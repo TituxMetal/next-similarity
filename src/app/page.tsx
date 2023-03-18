ba558eac2ce704780ebcdb34f39f2e05ad5558b4
Titux Metal <github@lgdweb.fr>
2023-03-18 04:05:04 +0100

    Home page (#12)
    * Add tailwincss animate and typography plugins
    
    * Add custom colors in tailwind config file
    
    * Add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,46 @@
-import { type NextPage } from 'next'
+import type { Metadata, NextPage } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Heading, Paragraph } from '~/components'
+
+export const metadata: Metadata = {
+  title: 'Similarity API | Home',
+  description: 'Free & Open-Source text similarity API'
+}
 
 const Home: NextPage = () => {
   return (
-    <main>
-      <h1 className='mb-4 text-3xl font-bold text-red-600 underline'>
-        Hello world!
-      </h1>
-    </main>
+    <div className='relative flex h-screen items-center justify-center overflow-x-hidden'>
+      <div className='container mx-auto h-full w-full max-w-7xl pt-32'>
+        <div className='flex h-full flex-col items-center justify-start gap-6 lg:items-start lg:justify-center'>
+          <Heading size='lg' className='text-dark-gold dark:text-light-gold'>
+            Easily determine <br /> text similarity
+          </Heading>
+          <Paragraph className='max-w-xl lg:text-left'>
+            With the Text Similarity API, you can easily determine the
+            similarity between two pieces of text with a free{' '}
+            <Link
+              href='/login'
+              className='text-black underline underline-offset-2 dark:text-light-gold'
+            >
+              API key
+            </Link>
+            .
+          </Paragraph>
+          <div className='relative aspect-square w-full max-w-xl lg:absolute lg:left-1/2 lg:max-w-xl 2xl:max-w-2xl'>
+            <Image
+              priority
+              className='img-shadow object-contain'
+              quality={100}
+              fill
+              src='/typewriter.png'
+              alt='typewriter.png'
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { colors } = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
@@ -6,9 +8,17 @@ module.exports = {
     extend: {
       container: {
         center: true,
-        padding: '1.5rem'
+        padding: '1.5rem',
+        screens: {
+          '2xl': '1360px'
+        }
+      },
+      colors: {
+        ...colors,
+        'light-gold': '#f5bc51',
+        'dark-gold': '#533519'
       }
     }
   },
-  plugins: []
+  plugins: [require('tailwindcss-animate', require('@tailwindcss/typography'))]
 }
